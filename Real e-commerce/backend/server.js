023b2f429817b4e1a4fbdaf84ddb0b61f92bc725
ChefDredz <bernardkinyua80@gmail.com>
2025-10-22 09:55:01 +0300

    Update CORS for production deployment

--- Real e-commerce/backend/server.js
+++ Real e-commerce/backend/server.js
@@ -4,7 +4,7 @@ import "dotenv/config";
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
 import userRouter from "./routes/UserRoute.js";
-import productRouter from "./routes/productRoute.js";
+import productRouter from "./routes/ProductRoute.js";
 
 // App Config
 const app = express();
